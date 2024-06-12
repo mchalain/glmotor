88ab1d55a90579529ef7aa991a44774e145f48aa
Chalain Marc <marc.chalain@gmail.com>
2024-06-12 13:44:05 +0200

    refactor egl initialization

--- src/surfaceegl.c
+++ src/surfaceegl.c
@@ -30,41 +30,35 @@ GLMOTOR_EXPORT GLMotor_t *glmotor_create(int argc, char** argv)
 	GLuint height = 480;
 	GLchar *name = "GLMotor";
 
+	egl_display = eglGetDisplay(native_display());
+
 	EGLNativeWindowType native_win;
 	native_win = native_createwindow(width, height, name);
 	if (native_win == 0)
 		return NULL;
 
-	egl_display = eglGetDisplay(native_display());
 	EGLint majorVersion;
 	EGLint minorVersion;
 	eglInitialize(egl_display, &majorVersion, &minorVersion);
+	warn("glmotor: egl %d.%d", majorVersion, minorVersion);
 
 	EGLint num_config;
 
 	/** window creation */
-	//eglBindAPI(EGL_OPENGL_API);
+#ifdef HAVE_GLESV2
+	eglBindAPI(EGL_OPENGL_ES_API);
+#else
+	eglBindAPI(EGL_OPENGL_API);
+#endif
 	eglGetConfigs(egl_display, NULL, 0, &num_config);
-#if 0
+
 	EGLint attributes[] = {
 		EGL_RED_SIZE, 8,
 		EGL_GREEN_SIZE, 8,
 		EGL_BLUE_SIZE, 8,
+		EGL_ALPHA_SIZE, 8,
 		EGL_NONE
 	};
-#else
-	EGLint attributes[] =
-	{
-		EGL_RED_SIZE,       5,
-		EGL_GREEN_SIZE,     6,
-		EGL_BLUE_SIZE,      5,
-		EGL_ALPHA_SIZE,     EGL_DONT_CARE,
-		EGL_DEPTH_SIZE,     EGL_DONT_CARE,
-		EGL_STENCIL_SIZE,   EGL_DONT_CARE,
-		EGL_SAMPLE_BUFFERS, 0,
-		EGL_NONE
-	};
-#endif
 	EGLConfig config;
 	eglChooseConfig(egl_display, attributes, &config, 1, &num_config);
 
@@ -86,11 +80,11 @@ GLMOTOR_EXPORT GLMotor_t *glmotor_create(int argc, char** argv)
 
 GLMOTOR_EXPORT GLuint glmotor_run(GLMotor_t *motor, GLMotor_Draw_func_t draw, void *drawdata)
 {
-	while (native_running(motor->surf->native_win))
+	do
 	{
-		draw(drawdata);
 		eglSwapBuffers(egl_display, motor->surf->egl_surface);
-	}
+		draw(drawdata);
+	} while (native_running(motor->surf->native_win));
 	return 0;
 }
 
