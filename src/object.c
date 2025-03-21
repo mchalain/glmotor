0ebfe1aad067e5865188e4fce173820d65536dd8
Chalain Marc <marc.chalain@gmail.com>
2025-03-21 16:47:32 +0100

    object: try to use half_float without any improvement

--- src/object.c
+++ src/object.c
@@ -17,6 +17,12 @@
 #include "glmotor.h"
 #include "log.h"
 
+/// the usage of half_float has no effect on the performances
+//#define VERTEX_HALFFLOAT
+#ifdef VERTEX_HALFFLOAT
+#include "halffloat.h"
+#endif
+
 /***********************************************************************
  * GLMotor_Object_t
  **********************************************************************/
@@ -64,8 +70,7 @@ static GLuint _object_setup(GLMotor_Object_t *obj, GLuint programID)
 
 	// assign vertices (points or positions)
 	glBindBuffer(GL_ARRAY_BUFFER, obj->ID[0]);
-	GLsizeiptr size = obj->maxpoints * sizeof(GLfloat) * 3;
-	glBufferData(GL_ARRAY_BUFFER, size, NULL, GL_STATIC_DRAW);
+	GLsizeiptr size = 0;
 	obj->position = glGetAttribLocation(programID, "vPosition");
 	if (obj->position < 0)
 	{
@@ -73,8 +78,15 @@ static GLuint _object_setup(GLMotor_Object_t *obj, GLuint programID)
 	}
 	else
 	{
+#ifdef VERTEX_HALFFLOAT
+		size = obj->maxpoints * sizeof(uint16_t) * 3;
+		glVertexAttribPointer(obj->position, 3, GL_HALF_FLOAT_OES, GL_FALSE, 0, (void*)0);
+#else
+		size = obj->maxpoints * sizeof(GLfloat) * 3;
 		glVertexAttribPointer(obj->position, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+#endif
 		glEnableVertexAttribArray(obj->position);
+		glBufferData(GL_ARRAY_BUFFER, size, NULL, GL_STATIC_DRAW);
 	}
 
 	if (obj->maxfaces)
@@ -97,12 +109,17 @@ static GLuint _object_setup(GLMotor_Object_t *obj, GLuint programID)
 
 	// assign uvs
 	glBindBuffer(GL_ARRAY_BUFFER, obj->ID[3]);
-	size = obj->maxpoints * sizeof(GLfloat) * 2;
-	glBufferData(GL_ARRAY_BUFFER, size, NULL, GL_STATIC_DRAW);
 	obj->uv = glGetAttribLocation(obj->programID, "vUV");
 	if (obj->uv >= 0)
 	{
+#ifdef VERTEX_HALFFLOAT
+		size = obj->maxpoints * sizeof(uint16_t) * 2;
+		glVertexAttribPointer(obj->uv, 2, GL_HALF_FLOAT_OES, GL_FALSE, 0, (void*)0);
+#else
+		size = obj->maxpoints * sizeof(GLfloat) * 2;
 		glVertexAttribPointer(obj->uv, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+#endif
+		glBufferData(GL_ARRAY_BUFFER, size, NULL, GL_STATIC_DRAW);
 		glEnableVertexAttribArray(obj->uv);
 	}
 
@@ -151,11 +168,26 @@ GLMOTOR_EXPORT GLint object_setprogram(GLMotor_Object_t *obj, GLuint programID)
 
 GLMOTOR_EXPORT GLuint object_appendpoint(GLMotor_Object_t *obj, GLuint npoints, GLfloat points[])
 {
+# define point_vectorsize 3
 	glBindBuffer(GL_ARRAY_BUFFER, obj->ID[0]);
 	GLuint offset = 0;
-	offset += obj->npoints * sizeof(GLfloat) * 3;
-	glBufferSubData(GL_ARRAY_BUFFER, offset, npoints * sizeof(GLfloat) * 3, points);
+#ifdef VERTEX_HALFFLOAT
+	for (int i = 0; i < npoints; i++)
+	{
+		uint16_t halfpoints[point_vectorsize];
+		for (int j = 0; j < point_vectorsize; j++)
+		{
+			halfpoints[j] = float16(points[j + i * point_vectorsize]);
+		}
+		offset += obj->npoints * sizeof(uint16_t) * point_vectorsize;
+		glBufferSubData(GL_ARRAY_BUFFER, offset, 1 * sizeof(uint16_t) * point_vectorsize, halfpoints);
+		obj->npoints++;
+	}
+#else
+	offset += obj->npoints * sizeof(GLfloat) * point_vectorsize;
+	glBufferSubData(GL_ARRAY_BUFFER, offset, npoints * sizeof(GLfloat) * point_vectorsize, points);
 	obj->npoints += npoints;
+#endif
 	return 0;
 }
 
@@ -363,11 +395,26 @@ GLMOTOR_EXPORT GLuint object_addtexture(GLMotor_Object_t *obj, GLMotor_Texture_t
 
 GLMOTOR_EXPORT GLuint object_appenduv(GLMotor_Object_t *obj, GLuint nuvs, GLfloat uvs[])
 {
+# define uv_vectorsize 2
 	glBindBuffer(GL_ARRAY_BUFFER, obj->ID[3]);
 	GLuint offset = 0;
-	offset += obj->nuvs * sizeof(GLfloat) * 2;
-	glBufferSubData(GL_ARRAY_BUFFER, offset, nuvs * sizeof(GLfloat) * 2, uvs);
+#ifdef VERTEX_HALFFLOAT
+	for (int i = 0; i < nuvs; i++)
+	{
+		uint16_t halfuvs[uv_vectorsize];
+		for (int j = 0; j < uv_vectorsize; j++)
+		{
+			halfuvs[j] = float16(uvs[j + i * uv_vectorsize]);
+		}
+		offset += obj->nuvs * sizeof(uint16_t) * uv_vectorsize;
+		glBufferSubData(GL_ARRAY_BUFFER, offset, 1 * sizeof(uint16_t) * uv_vectorsize, halfuvs);
+		obj->nuvs++;
+	}
+#else
+	offset += obj->nuvs * sizeof(GLfloat) * uv_vectorsize;
+	glBufferSubData(GL_ARRAY_BUFFER, offset, nuvs * sizeof(GLfloat) * uv_vectorsize, uvs);
 	obj->nuvs += nuvs;
+#endif
 	return 0;
 }
 
